adea1519b194934a8d5cb484dfd00459874b7862
Willie van Zyl <willem@glyphconnect.co.za>
2022-04-18 22:15:17 +0200

    Updated routes for / and /register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('registerUserRoute', [RegistrationController::class, 'registerUser'])->name('registerUser');

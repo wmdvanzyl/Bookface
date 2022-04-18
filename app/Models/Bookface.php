e8eefd280d8803912608a61d2b4609a1a33a1fd3
Willie van Zyl <willem@glyphconnect.co.za>
2022-04-18 22:12:55 +0200

    Initial check-in

---
+++ app/Models/Bookface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bookface extends Model
+{
+    use HasFactory;
+}

c8fec9d156585ecc976863bf3e1fac0b06b2cf0a
Willie van Zyl <willem@glyphconnect.co.za>
2022-04-19 16:39:01 +0200

    Minor logging changes

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -11,14 +11,13 @@ class RegistrationController extends Controller
     public function registerUser(Request $req) {
 
         $userRole = UserRole::where('level', 0)->pluck('name')->all()[0];
-        \Log::debug("User Role: ".$userRole);
 
         $newUser = new User();
         $newUser->name = $req->name;
         $newUser->email = $req->email;
         $newUser->password = $req->password;
         $newUser->role = $userRole;
-        \Log::debug($newUser);
+        \Log::debug('New User: '.$newUser);
         $newUser->save();
         return redirect('/');
     }
